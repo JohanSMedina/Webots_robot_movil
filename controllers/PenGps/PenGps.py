d306b15ddf29abeffad9bb6b39a46266880ae25a
JMedR <sebasmedina877@gmail.com>
2022-10-28 23:11:03 -0500

    Se agrega el prototipo de la funcion de detenccion del angulo de rotacion,la cual ya lo hace, solo que se deben cambiar los valores de entrada por los correctos

--- controllers/PenGps/PenGps.py
+++ controllers/PenGps/PenGps.py
@@ -1,16 +1,20 @@
 from controller import Robot, Motor
+import math
 pi = 3.14159265359
 pi2= pi*2
 
-#def defOri():
+def angleRot(x1,z1,x0,z0,nuevaPos):
+    global angle
+
+    angle = math.acos((x0*x1)+(z0*z1)/(math.sqrt((x0**2)+(z0**2))*math.sqrt((x1**2)+(z1**2))))
 
 
 
 def run_robot(robot):
+    global angle
 
-    trayectoria1 = [0.2,0.2],[0.2,1.8],[0.5,1.8]
-    flag  = 0
-    #print(trayectoria1[1][1])
+    nuevaPosicion = [0.5,0.2],[0.2,0.2],[0.2,1.8],[0.5,1.8]
+    flagAngle  = True
 
     xyz = [0,0,0]
     last_xz = [0,0]
@@ -46,17 +50,24 @@ def run_robot(robot):
         
         gps_value = gps.getValues() ###x, z, y
         
-        
-        for i in range(3):
+    
+        for i in range(3):##Llenado del vector con los valores del angulo, 6 decimales
             #xzy[i] = gps_value[i]
             xyz[i] = float("{:.6f}".format(gps_value[i]))
             
         print(f"Posicion en X: {xyz[0]}, Z: {xyz[2]}")
         
 
+        if (last_xz[0] != 0.0 and last_xz[1] != 0.0 and flagAngle == True):##Determinacion delñ angulo de giro
+            flagAngle = False
+
+            angleRot(xyz[0],xyz[2],last_xz[0],last_xz[1],nuevaPosicion[0],nuevaPosicion[1])
+            print(f"Angulo de rotacion deg: {angle}")
         
-        #defOri(xyz[0],xyz[2])
         
+        ##Reemplazo de los valores anteriores de X Z
+        last_xz[0] = xyz[0]
+        last_xz[1] = xyz[2]
         
         iMotor.setVelocity(max_speed*0.25)
         dMotor.setVelocity(max_speed*0.25)
