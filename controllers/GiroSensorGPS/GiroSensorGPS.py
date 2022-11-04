f581a59bc9ac9a5403117fb73859f236c32d0077
JohanSMedina <sebasmedina877@gmail.com>
2022-11-03 20:59:48 -0500

    Se agregan los ultimos cambios del contolador realizados en clase, pero toavia falta que se meva bien hacia la direccion que queremos

--- controllers/GiroSensorGPS/GiroSensorGPS.py
+++ controllers/GiroSensorGPS/GiroSensorGPS.py
@@ -4,6 +4,18 @@ import numpy
 import math
 
 def run_robot(robot):
+    #TODO Constantes
+    max_speed = math.pi
+    nuevaPosicion = [0.2,0.2],[0.1,0.2]
+    sizeNP = int(numpy.size(nuevaPosicion)/2)
+    posActual = 1
+    compararGiro = False
+    xz = [0,0]
+    last_xz = [0.2,0.1]
+    flagGiro = True
+    sDerecha = 0.0
+    sIzquierda = 0.0
+    
     ##TODO Instancas de los motores##
     iMotor = robot.getDevice('left wheel motor')
     dMotor = robot.getDevice('right wheel motor')
@@ -11,14 +23,6 @@ def run_robot(robot):
     iMotor.setVelocity(0.0)
     dMotor.setPosition(float('inf'))
     dMotor.setVelocity(0.0)
-
-    max_speed = math.pi
-    nuevaPosicion = [0.2,0.2],[0.2,0.5]
-    sizeNP = int(numpy.size(nuevaPosicion)/2)
-    posActual = 1
-    xz = [0,0]
-    last_xz = [0.1,0.2]
-    flagGiro = True
 
     ##TODO Creacion instancias del sensor##
     sIzquierda = robot.getDevice('left wheel sensor')
@@ -48,13 +52,19 @@ def run_robot(robot):
                     if(nuevaPosicion[posActual][0]<xz[0]):##* Girar a la dereccha x nueva menor qeu la actual
                         iMotor.setVelocity(max_speed)
                         dMotor.setVelocity(max_speed)
-                        iMotor.setPosition(sIzquierda.getValue()+3.14159265359)##TODO GIRO A LA DERECHA 90°
-                        dMotor.setPosition(sDerecha.getValue()-3.14159265359)
+                        sIzquierda = sIzquierda.getValue()+3.14159265359
+                        sDerecha = sDerecha.getValue()-3.14159265359
+                        print(f"sIzquierda: {sIzquierda}, sDerecha: {sDerecha}")
+                        iMotor.setPosition(sIzquierda)##TODO GIRO A LA DERECHA 90°
+                        dMotor.setPosition(sDerecha)
+
                     else:##* girar a la izquierda
                         iMotor.setVelocity(max_speed)
                         dMotor.setVelocity(max_speed)
                         iMotor.setPosition(sIzquierda.getValue()-3.14159265359)##TODO GIRO A LA IZQUIERDA 90°
                         dMotor.setPosition(sDerecha.getValue()+3.14159265359)
+                        sIzquierda = sIzquierda.getValue()
+                        sDerecha = sDerecha.getValue()
                 else:
 
                     if(nuevaPosicion[posActual][0]<xz[0]):##* Si la nueva x es mayor que la actual gira a la izquierda
@@ -62,11 +72,15 @@ def run_robot(robot):
                         dMotor.setVelocity(max_speed)
                         iMotor.setPosition(sIzquierda.getValue()+3.14159265359)##TODO GIRO A LA DERECHA 90°
                         dMotor.setPosition(sDerecha.getValue()-3.14159265359)
+                        sIzquierda = sIzquierda.getValue()
+                        sDerecha = sDerecha.getValue()
                     else:##*girar a la derecha
                         iMotor.setVelocity(max_speed)
                         dMotor.setVelocity(max_speed)
                         iMotor.setPosition(sIzquierda.getValue()+3.14159265359)##TODO GIRO A LA IZQUIERDA 90°
                         dMotor.setPosition(sDerecha.getValue()-3.14159265359)
+                        sIzquierda = sIzquierda.getValue()
+                        sDerecha = sDerecha.getValue()
             
             else:###* Sentido de giro si x es igual
 
@@ -77,31 +91,51 @@ def run_robot(robot):
                         dMotor.setVelocity(max_speed)
                         iMotor.setPosition(sIzquierda.getValue()-3.14159265359)##TODO GIRO A LA IZQUIERDA 90°
                         dMotor.setPosition(sDerecha.getValue()+3.14159265359)
+                        sIzquierda = sIzquierda.getValue()
+                        sDerecha = sDerecha.getValue()
                     else:#TODO GIRO A LA DERECHA 90°
                         iMotor.setVelocity(max_speed)
                         dMotor.setVelocity(max_speed)
                         iMotor.setPosition(sIzquierda.getValue()+3.14159265359)##TODO GIRO A LA DERECHA 90°
                         dMotor.setPosition(sDerecha.getValue()-3.14159265359)
+                        sIzquierda = sIzquierda.getValue()
+                        sDerecha = sDerecha.getValue()
                 else:
                     if(nuevaPosicion[posActual][1]<xz[1]):##TODO GIRO A LA DERECHA 90°
                         iMotor.setVelocity(max_speed)
                         dMotor.setVelocity(max_speed)
                         iMotor.setPosition(sIzquierda.getValue()+3.14159265359)##TODO GIRO A LA DERECHA 90°
                         dMotor.setPosition(sDerecha.getValue()-3.14159265359)
+                        sIzquierda = sIzquierda.getValue()
+                        sDerecha = sDerecha.getValue()
+
                     else:
                         iMotor.setVelocity(max_speed)
                         dMotor.setVelocity(max_speed)
                         iMotor.setPosition(sIzquierda.getValue()-3.14159265359)##TODO GIRO A LA IZQUIERDA 90°
                         dMotor.setPosition(sDerecha.getValue()+3.14159265359)
+                        sIzquierda = sIzquierda.getValue()
+                        sDerecha = sDerecha.getValue()
 
-            print(posActual)
+            #print(posActual)
+            #print(f"nuevaPosicion[posActual][0]: {nuevaPosicion[posActual][0]}, xz[0]: {xz[0]}, nuevaPosicion[posActual][1]: {nuevaPosicion[posActual][1]}, xz[1]: {xz[1]}")
             
             flagGiro = False
             posActual += 1
-            ##TODO Termina el codigo para los giros de orientacion
+            print(posActual)
+            ##TODO Termina el codigo para los giros de orientacio
         
-        if(flagGiro == False):
-            print("Aqui se debe mover para llegar al punto")
+        if(flagGiro==False and compararGiro == True):
+            saIzquierda = sIzquierda.getValue()
+            saDerecha = sDerecha.getValue()
+            print(f"saDerecha: {saDerecha}, sDerecha: {sDerecha}\nsIzquierda: {sIzquierda}, saIzquierda: {saIzquierda}")
+
+            if(saIzquierda == sIzquierda and saDerecha == sDerecha):
+                print("ya giró todo")
+        
+        
+        compararGiro = True
+        print("----------------------------------------------------------------------------------------")
 
 
 ### *Llamado a la funcion main##
